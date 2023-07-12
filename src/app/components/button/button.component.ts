aff926b038501f9b642cb2e06c3a1b23b1308201
kolusnk <skorus.jakub@gmail.com>
2023-07-12 12:03:47 +0200

    angular crush course

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+})
+export class ButtonComponent {
+  @Input() color: string;
+  @Input() text: string;
+  @Output() btnClick = new EventEmitter();
+  onClick() {
+    this.btnClick.emit();
+  }
+}
---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from 'src/app/Task';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.scss'],
+})
+export class TaskItemComponent {
+  @Input() task: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
+  onDelete(task: Task) {
+    this.onDeleteTask.emit(task);
+  }
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task);
+  }
+}
